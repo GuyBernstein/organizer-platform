9c2c16b1861d5cf9f22d13d852e51eef6f281658
shakshuka1998 <125633043+shakshuka1998@users.noreply.github.com>
2024-10-26 11:08:34 +0300

    with swagger

---
+++ src/main/java/com/enhanceai/platform/EnhanceAiPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.enhanceai.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnhanceAiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnhanceAiPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enhanceai/platform/EnhanceAiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enhanceai.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhanceAiPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
